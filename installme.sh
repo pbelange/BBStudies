2abe2a44827fd86e9d9c34d79bd272efae11cd33
pbelange <phbelang@cern.ch>
2023-10-13 23:02:37 +0200

    fixed installation to python3.10

--- installme.sh
+++ installme.sh
@@ -5,32 +5,41 @@ mkdir ./Executables
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform
     if [ "$(uname -m)" == "x86_64" ]; then
-        wget -O ./Executables/Miniconda3-latest.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+        wget -O ./Executables/Miniforge3-latest.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh
+
     elif [ "$(uname -m)" == "arm64" ]; then
-        wget -O ./Executables/Miniconda3-latest.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+        wget -O ./Executables/Miniforge3-latest.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh
     fi
 elif [ "$(uname)" == "Linux" ]; then
     # Do something under Linux platform
-    wget -O ./Executables/Miniconda3-latest.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    wget -O ./Executables/Miniforge3-latest.sh ./Executables/Miniconda3-latest.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 fi
-bash ./Executables/Miniconda3-latest.sh -b  -p ./Executables/miniconda -f
+bash ./Executables/Miniforge3-latest.sh -b  -p ./Executables/miniforge3 -f
 
 
 # create your own virtual environment in a new folder
-source ./Executables/miniconda/bin/activate
-python -m venv ./Executables/py_BB
-source ./Executables/py_BB/bin/activate
+source ./Executables/miniforge3/bin/activate
+
+conda update -n base -c conda-forge conda
+conda create -n py-BB python=3.10
+conda activate py-BB
+
+# FOR GPU Calculations
+#=======================================
+conda install mamba -n base -c conda-forge
+pip install cupy-cuda11x
+mamba install cudatoolkit=11.8.0
+pip install gpustat 
 
 
 # Install generic python packages
 #========================================
-pip install --upgrade pip
-pip install wheel
 pip install jupyterlab
 pip install ipywidgets
 pip install PyYAML
 pip install pyarrow
 pip install pandas
+pip install bokeh
 pip install matplotlib
 pip install scipy
 pip install ipympl
@@ -41,47 +50,50 @@ pip install pynaff
 pip install NAFFlib
 
 # Adding the jupyter kernel to the list of kernels
-python -m ipykernel install --user --name py_BB --display-name "py_BB"
-#========================================
+python -m ipykernel install --user --name py-BB --display-name "py-BB"
+# ========================================
 
 
 # Install CERN packages
 #=========================================
+# Installing fortran an other compilers
+conda install compilers cmake
+
 pip install cpymad
 
-git clone https://github.com/lhcopt/lhcmask.git ./Executables/py_BB/lhcmask
-pip install -e ./Executables/py_BB/lhcmask
+git clone https://github.com/lhcopt/lhcmask.git ./Executables/py-BB/lhcmask
+pip install -e ./Executables/py-BB/lhcmask
 
-git clone https://github.com/xsuite/xobjects ./Executables/py_BB/xobjects
-pip install -e ./Executables/py_BB/xobjects
+git clone https://github.com/xsuite/xobjects ./Executables/py-BB/xobjects
+pip install -e ./Executables/py-BB/xobjects
 
-git clone https://github.com/xsuite/xdeps ./Executables/py_BB/xdeps
-pip install -e ./Executables/py_BB/xdeps
+git clone https://github.com/xsuite/xdeps ./Executables/py-BB/xdeps
+pip install -e ./Executables/py-BB/xdeps
 
-git clone https://github.com/xsuite/xpart ./Executables/py_BB/xpart
-pip install -e ./Executables/py_BB/xpart
+git clone https://github.com/xsuite/xpart ./Executables/py-BB/xpart
+pip install -e ./Executables/py-BB/xpart
 
-git clone https://github.com/xsuite/xtrack ./Executables/py_BB/xtrack
-pip install -e ./Executables/py_BB/xtrack
+git clone https://github.com/xsuite/xtrack ./Executables/py-BB/xtrack
+pip install -e ./Executables/py-BB/xtrack
 
-git clone https://github.com/xsuite/xmask ./Executables/py_BB/xmask
-pip install -e ./Executables/py_BB/xmask
+git clone https://github.com/xsuite/xmask ./Executables/py-BB/xmask
+pip install -e ./Executables/py-BB/xmask
 
-git clone https://github.com/xsuite/xfields ./Executables/py_BB/xfields
-pip install -e ./Executables/py_BB/xfields
+git clone https://github.com/xsuite/xfields ./Executables/py-BB/xfields
+pip install -e ./Executables/py-BB/xfields
 
-git clone https://github.com/PyCOMPLETE/FillingPatterns.git ./Executables/py_BB/FillingPatterns
-pip install -e ./Executables/py_BB/FillingPatterns
+git clone https://github.com/PyCOMPLETE/FillingPatterns.git ./Executables/py-BB/FillingPatterns
+pip install -e ./Executables/py-BB/FillingPatterns
 
-git clone https://gitlab.cern.ch/mrufolo/fillingstudies.git ./Executables/py_BB/fillingstudies
-#pip install -e ./Executables/py_wireDAQ/fillingstudies
+git clone https://gitlab.cern.ch/mrufolo/fillingstudies.git ./Executables/py-BB/fillingstudies
+pip install -e ./Executables/py-BB/fillingstudies
 #=========================================
 
 
 
 # Download outsourced files
 #=========================================
-cd ./Executables/py_BB/xmask
+cd ./Executables/py-BB/xmask
 git submodule init
 git submodule update
 
